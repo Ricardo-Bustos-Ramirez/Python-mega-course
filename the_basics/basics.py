ac888899dfcba3ac8af29e706fdff2dbfb310576
Ricardo Bustos-Ramirez <ricardo.bustos.rmz@gmail.com>
2020-08-03 14:16:11 -0400

    Files added until course 30. Accessing Items and Slides with Negative Indexes.

---
+++ the_basics/basics.py
@@ -0,0 +1,39 @@
+# import datetime
+# mynow = datetime.datetime.now()
+# print(mynow)
+
+# mynumber = 10
+# mytext = "Hello world!"
+
+# print(mynumber, mytext)
+
+# x = 10
+# y = "10"
+# z = 10.1
+# z2 = -7.1e-12
+
+# sum1 = x + x
+# sum2 = y + y
+# print(sum1, sum2)
+# print(type(x), type(y), type(z), type(z2))
+
+# student_grades = {"Meli": 9.1, "Ricardo": 8.8, "Laura": 7.5}
+
+# my_sum = sum(student_grades.values())
+# length = len(student_grades)
+# mean = my_sum/length
+# print(mean)
+# print(student_grades.keys())
+
+# monday_temperatures = (1, 4, 5)
+# print(monday_temperatures)
+
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+monday_temperatures.__getitem__(1)
+print(monday_temperatures[1:3])
+print(monday_temperatures[:2])
+print(monday_temperatures[3:])
+print(monday_temperatures[-1])
+print(monday_temperatures[-2:])
+
+
---
+++ the_basics/exercise.py
@@ -0,0 +1,2 @@
+x = "Meli y Ricardo"
+print(x)
