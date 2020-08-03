22e108f474a3166dc6cdce9fe0bcb0dcb5d372cf
Ricardo Bustos-Ramirez <ricardo.bustos.rmz@gmail.com>
2020-08-03 18:02:30 -0400

    End of session of functions.

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,43 +1,10 @@
-# import datetime
-# mynow = datetime.datetime.now()
-# print(mynow)
+def mean(mylist):
+    print("Function started!")
+    the_mean = sum(mylist) / len(mylist)
+    return the_mean
 
-# mynumber = 10
-# mytext = "Hello world!"
+print(mean([1, 4, 5]))
+print(type(mean), type(sum))
 
-# print(mynumber, mytext)
-
-# x = 10
-# y = "10"
-# z = 10.1
-# z2 = -7.1e-12
-
-# sum1 = x + x
-# sum2 = y + y
-# print(sum1, sum2)
-# print(type(x), type(y), type(z), type(z2))
-
-# student_grades = {"Meli": 9.1, "Ricardo": 8.8, "Laura": 7.5}
-
-# my_sum = sum(student_grades.values())
-# length = len(student_grades)
-# mean = my_sum/length
-# print(mean)
-# print(student_grades.keys())
-
-# monday_temperatures = (1, 4, 5)
-# print(monday_temperatures)
-
-# monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
-# monday_temperatures.__getitem__(1)
-# print(monday_temperatures[1:3])
-# print(monday_temperatures[:2])
-# print(monday_temperatures[3:])
-# print(monday_temperatures[-1])
-# print(monday_temperatures[-3:])
-# monday_temperatures = ['hello', 1, 2, 3]
-
-student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
-eng_port = {"rain": "chuva", "sun": "sol"}
-
-
+my_mean = mean([0, 3, 4])
+print(my_mean + 10)
