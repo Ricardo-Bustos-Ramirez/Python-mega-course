d7e798b993a96e654be9e71569818635f0cbf711
Ricardo Bustos-Ramirez <ricardo.bustos.rmz@gmail.com>
2020-08-17 17:30:54 -0400

    43. White Space

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,11 +1,17 @@
-def mean(value):
-    if isinstance(value, dict):
-        the_mean = sum(value.values()) / len(value)
-    else:
-        the_mean = sum(value) / len(value)
-    return the_mean
+if      3>1:
+    print('a')
 
-student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
-monday_temperatures = [8.8, 9.1, 9.9]
-print(mean(student_grades))
-print(mean(monday_temperatures))
+print('aa')
+print('aaa')
+
+if 3>1:
+    print('b')
+
+print('bb')
+print('bbb')
+
+if 3 > 1:
+    print('c')
+
+print('cc')
+print('ccc')
