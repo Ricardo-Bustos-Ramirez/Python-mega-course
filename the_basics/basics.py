76f0e1733e016b658a49bee161f54d6805b2e532
Ricardo Bustos-Ramirez <ricardo.bustos.rmz@gmail.com>
2020-08-18 12:57:00 -0400

    47. String Formatting with Multple Variables

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,5 +1,9 @@
-user_input = input("Enter your name: ")
-message_1 = "Hello %s!" % user_input    # This works for both Python 2 and 3
-message_2 = f"Hello {user_input}"       # New way for Python 3.6
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+when = "today"
+
+message_1 = "Hello %s %s!" % (name, surname)    # This works for both Python 2 and 3
+message_2 = f"Hello {name} {surname}. What's up {when}?"       # New way for Python 3.6
+
 print(message_1)
 print(message_2)
